4d28814348ea5471b640d801af8691d85b89bd6d
Donghee Nam <63303@cgs.act.edu.au>
2021-10-08 10:43:52 +1100

    minor fix

--- smarties/index.js
+++ smarties/index.js
@@ -10,22 +10,27 @@ function randInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// returns a <span> that draws Smarty of the specified kind (as index)
-function makeSmarty(index) {
-  return `<span class='smarties smarties-${sName[index]} m-1'>${sLeg[index]}</span>`;
+
+
+// returns random Smartie <span>
+function makeSmarty() {
+  // name and legend of Smarties
+  const sName = ['green', 'red', 'orange', 'blue', 'pink', 'yellow', 'purple', 'brown'];
+  const sLeg = ['G', 'R', 'O', 'B', 'I', 'Y', 'U', 'N'];
+
+  // choose a random index
+  let i = randInt(0, sName.length-1);
+  return `<span class='smarties smarties-${sName[i]} m-1'>${sLeg[i]}</span>`;
 }
 
 // creates a random number of random Smarties inside #container-smarties
 function makeSmarties() {
   const d = document.getElementById("container-smarties");
-
-  // name and legend of Smarties
-  const sName = ['green', 'red', 'orange', 'blue', 'pink', 'yellow', 'purple', 'brown'];
-  const sLeg = ['G', 'R', 'O', 'B', 'I', 'Y', 'U', 'N'];
-
   d.innerHTML = "";
+
+  // number of Smarties
   let no = gaussian(12.5, 1);
   for (let i=0; i<no; i++) {
-    d.innerHTML += makeSmarty(randInt(0,sName.length-1));
+    d.innerHTML += makeSmarty();
   }
 }
