ad69bafb18d8c5bf1f28530bcbfd7a5ff77015f9
Donghee Nam <donghee.nam04@gmail.com>
2023-02-15 09:26:42 +1100

    Improve clarity on help

--- python/bernoulli.py
+++ python/bernoulli.py
@@ -1,6 +1,6 @@
-""" meausres the chance of at least one success from (n_exp) experiments, 
-    with chance of success = 1/(n_exp),
-    by repeating the trial (n_trial) times """
+""" 
+calculates the chance of failing a Bernoulli trial with n-sided dice
+"""
 import argparse
 
 import numpy as np
@@ -12,26 +12,26 @@ def trial(n) :
 
 if __name__ == "__main__" :
     parser = argparse.ArgumentParser()
-    parser.add_argument("n_exp", type=int, nargs="?", default=200)
-    parser.add_argument("n_trials", type=int, nargs="?", default=30000)
+    parser.add_argument("d", type=int, nargs="?", default=200, help="number of sides of the dice")
+    parser.add_argument("trials", type=int, nargs="?", default=300000, help="number of trials for the simulation")
     args = parser.parse_args()
 
-    n_exp = args.n_exp          # number of experiments for each trial (p = 1/n)
-    n_trials = args.n_trials    # number of trials
+    d = args.d              # number of experiments for each trial (p = 1/n)
+    trials = args.trials    # number of trials
 
-    print(f"For each trial, {n_exp} experiments are conducted.")
-    print(f"Probability of success is 1/{n_exp}.")
+    print(f"For each trial, a {d}-sided die is thrown {d} times.")
+    print(f"The trial succeeds if 1 is rolled at least once.")
     successes = 0
     fails = 0
-    for i in range(n_trials) :
-        if (trial(n_exp)) :
+    for i in range(trials) :
+        if (trial(d)) :
             successes += 1
         else :
             fails += 1
     print("===================================================")
-    print(f"Number of trials: {n_trials}")
+    print(f"Number of trials: {trials}")
     print(f"successes       : {successes}")
     print(f"fails           : {fails}")
-    print(f"P(fail)         : {fails/n_trials}")
-    print(f"1/P(fail)       : {n_trials/fails}")
+    print(f"P(fail)         : {fails/trials}")
+    print(f"1/P(fail)       : {trials/fails}")
     print("===================================================")
